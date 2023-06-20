c2f7f4327d766a9bd2a8ccbe318675d4a0b2728c
Mauro Gomez <gomezmejiam@gmail.com>
2023-06-20 10:53:45 -0500

    Test del work flow

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = require('./holamundo');
 const port = process.env.PORT || 3000;
 
 app.listen(port,
-    () => console.log(`La aplicación está corriendo en http://localhost:${port}`)
+    () => console.log(`El servicio está corriendo en http://localhost:${port}`)
 );
