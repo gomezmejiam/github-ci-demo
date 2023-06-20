68f09a5540403ad8c1c6194b6fb793a1333633af
Mauro Gomez <gomezmejiam@gmail.com>
2023-06-20 12:39:03 -0500

    Agregar servicio de multiplicacion

--- services.js
+++ services.js
@@ -19,4 +19,12 @@ services.get('/resta', (req, res) => {
   res.send(`La resta de ${num1} y ${num2} es ${suma}`);
 });
 
+services.get('/multiplicacion', (req, res) => {
+  const num1 = parseInt(req.query.numero1);
+  const num2 = parseInt(req.query.numero2);
+  const suma = num1 * num2;
+
+  res.send(`La multiplicacion de ${num1} y ${num2} es ${suma}`);
+});
+
 module.exports = services;
