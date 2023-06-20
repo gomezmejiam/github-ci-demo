04fb778ded696d8358d9efc638da222d7d7ca6de
Mauro Gomez <gomezmejiam@gmail.com>
2023-06-20 10:35:12 -0500

    Inicial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const app = require('./holamundo');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,
+    () => console.log(`La aplicación está corriendo en http://localhost:${port}`)
+);
