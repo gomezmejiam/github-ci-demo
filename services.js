51f89def64be2b8525fcb5308fb4416d20d2fc67
Mauro Gomez <gomezmejiam@gmail.com>
2023-06-20 12:16:38 -0500

    Agregar servicio de operaciones

---
+++ services.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const services = express();
+
+services.get('/', (req, res) => res.send('Hola Mundo!'));
+
+services.get('/suma', (req, res) => {
+  const num1 = parseInt(req.query.numero1);
+  const num2 = parseInt(req.query.numero2);
+  const suma = num1 + num2;
+
+  res.send(`La suma de ${num1} y ${num2} es ${suma}`);
+});
+
+services.get('/resta', (req, res) => {
+  const num1 = parseInt(req.query.numero1);
+  const num2 = parseInt(req.query.numero2);
+  const suma = num1 - num2;
+
+  res.send(`La resta de ${num1} y ${num2} es ${suma}`);
+});
+
+module.exports = services;
